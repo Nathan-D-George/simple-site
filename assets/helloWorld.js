fe45b05f7450d8662c483ed8e0b9daaa8cff0ef3
Nathan-D-George <natedan2109@gmail.com>
2025-12-18 23:44:59 +0200

    toggle images

---
+++ assets/helloWorld.js
@@ -0,0 +1,27 @@
+class HelloWorld extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    conectedCallback() {
+        this.querySelectorAll('button').forEach(button =>    
+            button.addEventListener('click', this.onClick.bind(button))
+        );
+
+    }
+
+    onClick() {
+        const imageToShowId = `hidden${this.id.replace('show','')}`;
+        const imageToShow = document.getElementById(imageToShowId);
+        
+        if (!imageToShow) {
+            return;
+        }
+
+        imageToShow.style.display == 'none'
+            ? imageToShow.style.display = 'block'
+            : imageToShow.style.display = 'none';
+    }
+}
+
+customElements.define('hello-world', HelloWorld);
